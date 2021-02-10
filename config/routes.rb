163bd5b7746ce85cc48db8974ef0015f810429ec
markdrehmann <markdrehmann@gmail.com>
2021-02-10 16:47:53 -0600

    edit routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :rosters
   resources :courts
   resources :players
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
 end
