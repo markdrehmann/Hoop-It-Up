e148865b4e95b6a82fb51db31af56814c533ca3b
markdrehmann <markdrehmann@gmail.com>
2021-02-11 21:31:13 -0600

    added logout to nav if current player exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_player
     def current_player
-      Player.find(session[:player_id])
+      Player.find(session[:player_id]) if logged_in?
     end
   
     helper_method :logged_in?
