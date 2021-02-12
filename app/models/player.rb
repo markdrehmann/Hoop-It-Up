9e56df28f649ebaa411ef706e82bdd7308c1337a
markdrehmann <markdrehmann@gmail.com>
2021-02-12 11:08:08 -0600

    added validation to player - email presence true

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,7 @@ class Player < ApplicationRecord
     has_secure_password
     has_many :player_rosters
     has_many :rosters, through: :player_rosters
-    validates :email, uniqueness: true
+    validates :email, uniqueness: true, presence: true
     validates :password, presence: true
     validates :name, presence: true
 end
