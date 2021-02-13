fedeb75917841cd8e2fdb5b3a0dbce9ab1ce372e
markdrehmann <markdrehmann@gmail.com>
2021-02-13 13:52:11 -0600

    create new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,5 +6,28 @@ class GamesController < ApplicationController
     def show
         @game = Game.find(params[:id])
     end
+
+    def new
+        @game = Game.new
+    end
+
+    def create
+        @game = Game.new(game_params)
+        roster = Roster.create
+        roster.players << current_player
+        @game.roster_id = roster.id
+        if @game.save
+            flash[:alert] = "New Game Created!"
+            redirect_to player_path(current_player)
+        else
+            flash[:error] = @game.errors.full_messages.to_sentence
+            render :new
+        end
+    end
     
+    private
+
+    def game_params
+        params.require(:game).permit(:court_id, :time, :roster_id)
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
   belongs_to :court
   belongs_to :roster
+  validates :court_id, presence: true
+  validates :roster_id, presence: true
+  validates :time, presence: true
 
   def gametime
     time.to_formatted_s(:gametime)
