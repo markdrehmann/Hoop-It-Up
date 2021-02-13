e9724cda0ecf446924bdf5e0038ca9f7b83db238
markdrehmann <markdrehmann@gmail.com>
2021-02-13 10:45:14 -0600

    can't join game you're already in

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -2,8 +2,14 @@ class RostersController < ApplicationController
 
     def edit
         roster = Roster.find(params[:id])
-        roster.players << current_player
-        redirect_to player_path(current_player)
+        if roster.players.include?(current_player)
+            flash[:error] = "You're already in that game, sucka"
+            redirect_to games_path
+        else
+            roster.players << current_player
+            flash[:alert] = "You're in!"
+            redirect_to player_path(current_player)
+        end
     end
 
 end
