474c26a3cc3fab4c4c903946a38e963f5541d25c
markdrehmann <markdrehmann@gmail.com>
2021-02-13 10:41:32 -0600

    players can join games

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,2 +1,9 @@
 class RostersController < ApplicationController
+
+    def edit
+        roster = Roster.find(params[:id])
+        roster.players << current_player
+        redirect_to player_path(current_player)
+    end
+
 end
