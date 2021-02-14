ce3675781f4af8776324e79e6f56cf1b58507019
markdrehmann <markdrehmann@gmail.com>
2021-02-13 21:50:59 -0600

    stub out new court, show games in court show view

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -6,4 +6,18 @@ class CourtsController < ApplicationController
     def show
         @court = Court.find(params[:id])
     end
+
+    def new
+        @court = Court.new
+    end
+
+    def create
+        raise params.inspect
+    end
+
+    private
+
+    def court_params
+        params.require(:court).permit(:name, :address)
+    end
 end
