4b7e42f04c0dcccc32befd3c31b8270414f35432
markdrehmann <markdrehmann@gmail.com>
2021-02-15 13:18:44 -0600

    bashing my head against the wall with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     end
 
     def create
+        raise params.inspect
         @player = Player.find_by(email: params[:email])
         if @player && @player.authenticate(params[:password])
             session[:player_id] = @player.id
