494353452e6a9c303e08576da1eb1fb93031edfa
markdrehmann <markdrehmann@gmail.com>
2021-02-15 20:41:45 -0600

    refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  resources :players
-  resources :games
+  # resources :players
+  resources :players, only: [:new, :create, :show]
+  # resources :games
+  resources :games, only: [:new, :create, :index, :show]
   # resources :rosters
   resources :rosters, only: [:show, :edit]
   resources :courts do
