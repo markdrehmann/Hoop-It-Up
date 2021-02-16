18b30a5bac00c9f44dd0a79cfef34d05cd2f0df9
markdrehmann <markdrehmann@gmail.com>
2021-02-15 20:46:10 -0600

    editing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  # resources :players
   resources :players, only: [:new, :create, :show]
-  # resources :games
   resources :games, only: [:new, :create, :index, :show]
-  # resources :rosters
   resources :rosters, only: [:show, :edit]
   resources :courts do
     resources :games, only: [:new, :index]
