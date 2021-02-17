d8a117d80603805769b73622501485af1aa055b5
markdrehmann <markdrehmann@gmail.com>
2021-02-16 20:03:27 -0600

    refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,35 @@
 class SessionsController < ApplicationController
+  def login
+  end
 
-    def login
-    end
+  def create
+    auth_hash = request.env["omniauth.auth"]
+    auth_hash ? login_with_auth(auth_hash) : login
+  end
 
-    def create
-        auth_hash = request.env["omniauth.auth"]
-        if auth_hash
-            if  player = Player.find_by(email: auth_hash.uid)
-                session[:player_id] = player.id
-                redirect_to player_path(player)
-            else
-                p = Player.create(email: auth_hash.uid, name: auth_hash[:info][:nickname], password: SecureRandom.hex)
-                p.save
-                session[:player_id] = p.id
-                redirect_to player_path(p)
-            end
-        else
-            @player = Player.find_by(email: params[:email])
-            if @player && @player.authenticate(params[:password])
-                session[:player_id] = @player.id
-                flash[:alert] = "Logged in as #{@player.name}!"
-                redirect_to player_path(@player)
-            else
-                flash[:error] = "Invalid Login!"
-                redirect_to :login
-            end
-        end
-    end
+  def logout
+    session.clear
+    redirect_to root_path
+  end
 
-    def logout
-        session.clear
-        redirect_to root_path
+  private
+
+  def login_with_auth(auth_hash)
+    player = Player.find_by(email: auth_hash.uid)
+    player ||= Player.create(email: auth_hash.uid, name: auth_hash[:info][:nickname], password: SecureRandom.hex)
+    session[:player_id] = player.id
+    redirect_to player_path(player)
+  end
+
+  def login
+    player = Player.find_by(email: params[:email])
+    if player && player.authenticate(params[:password])
+      session[:player_id] = player.id
+      flash[:alert] = "Logged in as #{player.name}!"
+      redirect_to player_path(player)
+    else
+      flash[:error] = "Invalid Login!"
+      redirect_to :login
     end
+  end
 end
